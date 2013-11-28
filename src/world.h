9f39b7ee677802c4cf7d2948dd6e2194e749616c
Zer0XoL <zero_koll@hotmail.com>
2013-11-28 17:48:22 +0100

    implemented world

--- src/world.h
+++ src/world.h
@@ -9,33 +9,40 @@
 class World
 {
 public:
-	World()
-	{
-		//
-	}
-	~World()
-	{
-		//
-	}
+	World();
+	~World();
 	
 	void clear();
 	void setRoom(Position position, Room room);
 	void removeRoom(Position position);
 	std::vector<std::string> getRoomNames() const
 	{
-		//
+		std::vector<std::string> names;
+		for(auto it = m_rooms.begin(); it != m_rooms.end(); it++)
+		{
+			names.push_back( it->first.getName() );
+		}
+		return names;
 	}
 	std::vector<Position> getRoomPositions() const
 	{
-		//
+		std::vector<Position> positions;
+		for(auto it = m_rooms.begin(); it != m_rooms.end(); it++)
+		{
+			positions.push_back( it->second );
+		}
+		return positions;
 	}
 	Room getRoom(Position position) const
 	{
 		return m_rooms[position];
 	}
-	bool hasRoom(Position) const
+	bool hasRoom(Position position) const
 	{
-		//
+		auto it = m_rooms.find( position );
+		if(it != m_rooms.end())
+			return true;
+		return false;
 	}
 private:
 	std::map<Position, Room> m_rooms;
