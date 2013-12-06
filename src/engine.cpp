bfbe92066630479e5868f405c2e4496af2753ee8
Zer0XoL <zero_koll@hotmail.com>
2013-12-06 21:39:00 +0100

    fixade buggar i engine

--- src/engine.cpp
+++ src/engine.cpp
@@ -188,7 +188,7 @@ bool Engine::MakeItem(vector<string> arg_list)
 	Item temp(item_name);
 	for(int i=1;i<arg_list.size();++i)
 	{
-		switch(arg_list[i])
+		switch(arg_list[i][0])
 		{
 			case 'e'://edible
 				temp.addProperty(eProperty::edible);
@@ -203,7 +203,7 @@ bool Engine::MakeItem(vector<string> arg_list)
 			break;
 		}
 	}
-	m_world.getRoom( m_player.getPosition(), temp);
+	m_world.setRoom( m_player.getPosition(), temp);
 	return true;
 }
 
